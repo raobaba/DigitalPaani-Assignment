1edeb52ded9d75671869695594aa5d5bfd11b412
Rajan Kumar <raorajan9576@gmail.com>
2024-04-25 17:37:15 +0530

    server is running

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Server is Running! 🚀");
+});
+
+module.exports = app;
