9e95d10680e59155bffb492aab03f5ace4a3766f
Rajan Kumar <raorajan9576@gmail.com>
2024-04-25 17:40:30 +0530

    mongoose connected successfully

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-
+const Connection = require('./config/db')
 app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-
+Connection();
 
 
 app.get("/", (req, res) => {
