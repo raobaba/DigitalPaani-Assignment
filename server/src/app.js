22eb51a2b9d92a4a3c5ac81f1bf77c34e1c11a94
Rajan Kumar <raorajan9576@gmail.com>
2024-04-25 18:27:32 +0530

    register api created

--- server/src/app.js
+++ server/src/app.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const Connection = require('./config/db')
-app.use(express.static("public"));
+const userRouter = require('./routes/user.route')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 Connection();
-
+app.use('/api/v1',userRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is Running! 🚀");
---
+++ server/src/utils/errorHandler.js
@@ -0,0 +1,7 @@
+function ErrorHandler(message, statusCode) {
+    const error = new Error(message);
+    error.statusCode = statusCode;
+    return error;
+}
+
+module.exports = ErrorHandler;
