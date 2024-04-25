bf5d8aae70ac07e21d267cf28ea47d976a06269a
Rajan Kumar <raorajan9576@gmail.com>
2024-04-25 17:49:47 +0530

    model created for user

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    fullname: {
+        type: String,
+        required: [true, "Please Enter Your FullName"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please Enter Your Email"],
+        unique: true,
+        validate: [validator.isEmail, "Please enter a valid email address"]
+    },
+    gender: {
+        type: String,
+        required: [true, "Please Enter Gender"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please Enter Your Password"],
+        minlength: [8, "Password should have at least 8 characters"],
+        select: false,
+    },
+    role: {
+        type: String,
+        default: "user",
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.getJWTToken = function () {
+    return jwt.sign({id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+}
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.methods.getResetPasswordToken = async function () {
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+    this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+    return resetToken;
+}
+
+const User = model('User', userSchema);
+
+export default User;
