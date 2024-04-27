98e5c5fe85088d208c17657f7de87f716b3e4761
Rajan Kumar <raorajan9576@gmail.com>
2024-04-27 23:43:04 +0530

    weather forecase design completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Details from "./components/Details";
+import WeatherForecast from "./components/WeatherForecast";
 
 function App() {
   return (
-   <>
-   <Navbar/>
-   <Details/>
-   </>
+    <>
+      <Navbar />
+      <Details />
+      <WeatherForecast />
+    </>
   );
 }
 
