7e2fb3965fa67f7c234fab9288d0ced5645b2b18
Rajan Kumar <raorajan9576@gmail.com>
2024-04-28 00:12:52 +0530

    fixes the search icon style

--- weather_app/src/components/Navbar.jsx
+++ weather_app/src/components/Navbar.jsx
@@ -14,15 +14,31 @@ function Navbar() {
           </span>
         </a>
         <div class="flex md:order-2">
-          <div class="">
+          <div class="relative">
             <input
               type="text"
               id="search-navbar"
-              class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="block w-full p-3 pl-4 pr-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Enter City Name..."
             />
+            <svg
+              id="search-icon"
+              class="absolute w-6 h-6 text-gray-400 top-3 right-3 cursor-pointer"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="2"
+                d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+              />
+            </svg>
           </div>
         </div>
+
         <div
           class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-search"
